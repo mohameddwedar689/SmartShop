253d92775163c67fe6d9f3d3159afd7a33cb6cb6
mohameddwedar689 <mohameddwedar689@gmail.com>
2025-01-10 03:11:53 +0200

    [create] back-end user authentication(with JWT - simpleJWT)

---
+++ backend/base/urls/user_urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from base.views import user_views as views
+from rest_framework_simplejwt.views import (TokenObtainPairView)
+
+urlpatterns = [
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/' , views.getUserProfile , name='user-profile'),
+    path('register/' , views.registerUser , name='register'), 
+    path('' , views.getUsers , name='users'), 
+]
